834b72f154ab33f47d04eb69ad96f2ddd39cb285
Paproc <too173poo@gmail.com>
2026-01-11 15:57:29 +0100

    visitor

---
+++ Visitor.cs
@@ -0,0 +1,12 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ZTP;
